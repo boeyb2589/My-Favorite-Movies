dbf8af68619ff2a58e19ac326c96440bc4cd1183
Brandon Boey <brandonjboey@gmail.com>
2020-04-16 12:55:01 -0400

    Initial Commit

---
+++ My Favorite Movies/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  My Favorite Movies
+//
+//  Created by Brandon Boey on 4/16/20.
+//  Copyright © 2020 Brandon Boey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
